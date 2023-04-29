8b4f8c90cb89afba51f7c453e473fa69013eeca2
CorsairRom <ric.romero@duocuc.cl>
2023-04-29 03:40:12 -0400

    ADD: modal delete

---
+++ src/components/ModalDelete.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="body-modal" v-if="showModalDelete">
+        <div class="modal-card" v-if="showModalDelete">
+            <div class="modal-btn" @click="closeModalDelete()">
+                <p>X</p>
+            </div>
+            <h1>{{ showModalDelete }}</h1>
+        </div>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+let showModalDelete = ref(true);
+const closeModalDelete = () =>{
+    showModalDelete.value = false;
+    console.log(showModalDelete);
+}
+</script>
+<style scoped lang="css">
+    .modal-card{
+        position: absolute;
+        width: 900px;
+        height: 227px;
+        left: 414px;
+        top: 445px;
+        z-index: 9999;
+        /* Blanco calido */
+
+        background: #FBFBFB;
+        /* card shadow */
+
+        box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.07);
+        border-radius: 15px;
+    }
+</style>
