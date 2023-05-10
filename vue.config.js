7fb6d307ff8acf1e1330bff032ba15322ab0c5b8
CorsairRom <101121940+CorsairRom@users.noreply.github.com>
2023-05-09 21:44:43 -0400

    Update vue.config.js
    restart

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
 })
